81b63a174991a0c7d6cf05e72663460bd5037ae6
Jeremy Montes <jeremymontes@pursuit.org>
2023-12-18 09:33:54 -0500

    added reduce problem

---
+++ Reduce/reduce-transformation.js
@@ -0,0 +1,26 @@
+
+// Given an integer array nums, a reducer function fn, and an initial value init, return a reduced array.
+
+// A reduced array is created by applying the following operation: val = fn(init, nums[0]), val = fn(val, nums[1]), val = fn(val, nums[2]), ... until every element in the array has been processed. The final value of val is returned.
+
+// If the length of the array is 0, it should return init.
+
+// Please solve it without using the built-in Array.reduce method.
+
+//  * @param {number[]} nums
+//  * @param {Function} fn
+//  * @param {number} init
+//  * @return {number}
+ 
+var reduce = function(nums, fn, init) {
+
+    let counter = init
+
+  for(let num of nums){
+
+counter =  fn(counter, num) 
+
+  }
+  
+  return counter
+    }
